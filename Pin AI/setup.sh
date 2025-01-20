d1df0c915130c247047005ffa7d07bba08074057
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-20 14:42:21 +0700

    Update setup.sh

--- Pin AI/setup.sh
+++ Pin AI/setup.sh
@@ -85,7 +85,7 @@ while true; do
 
             check_configs
 
-            for file in datas.txt wallets.txt proxies.txt; do
+            for file in datas.txt wallets.txt proxies.txt tokens.json; do
                 if [ ! -f "$file" ]; then
                     touch "$file"
                     print_green "Created $file"
