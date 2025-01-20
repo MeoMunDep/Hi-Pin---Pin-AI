a417bddd3ce804b3b44b3aa40a97a81caa1618f6
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-20 14:41:47 +0700

    Update setup.bat

--- Pin AI/setup.bat
+++ Pin AI/setup.bat
@@ -65,6 +65,10 @@ if not exist proxies.txt (
     type nul > proxies.txt
     echo Created proxies.txt
 )
+if not exist proxies.txt (
+    type nul > tokens.txt
+    echo Created tokens.txt
+)
 
 echo.
 echo Configuration files have been created/checked.
